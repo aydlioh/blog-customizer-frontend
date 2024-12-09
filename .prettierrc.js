27db67985d2fd53e3aa4085b0366e3ef2a945cb5
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-12-09 19:16:19 +0500

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	semi: true,
+	trailingComma: 'es5',
+	singleQuote: true,
+	jsxSingleQuote: true,
+	bracketSameLine: true,
+	printWidth: 80,
+	tabWidth: 2,
+	endOfLine: 'lf',
+};
